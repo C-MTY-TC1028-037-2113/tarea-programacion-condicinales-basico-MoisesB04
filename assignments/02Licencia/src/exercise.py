9664182d28c7756874dac7a25736fbfdf8a5f2f6
Moisés Benavides <A01178037@itesm.mx>
2021-08-30 15:18:42 -0500

    Tarea condicionales básico

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,6 +2,19 @@
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    if (edad >= 18):
+        identificacion = str(input("¿Tienes identificación oficial? (s/n): "))
+        if (identificacion == "s"):
+            print("Trámite de licencia concedido")
+        elif (identificacion == "n"):
+            print("No cumples requisitos")  
+        else: 
+            print("Respuesta incorrecta")
+    elif (edad > 0):
+        print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
+
 
 if __name__ == '__main__':
     main()
--- assignments/06BMI/README.md
+++ assignments/06BMI/README.md
@@ -3,7 +3,7 @@
 
 Modifica el programa que se encuentra en la carpeta `src` que se llama
 `exercise.py` y que contiene el siguiente código:
-
+ 
 ```python
 
 def main():
